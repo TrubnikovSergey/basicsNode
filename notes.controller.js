dddbff8c978aa5dc83a0f23a1afc56859830717f
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-11-22 15:42:03 +0300

    home work finished

--- notes.controller.js
+++ notes.controller.js
@@ -1,40 +1,60 @@
-const fs = require('fs/promises')
-const path = require('path')
-const chalk = require('chalk')
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
 
-const notesPath = path.join(__dirname, 'db.json')
+const notesPath = path.join(__dirname, "db.json");
 
 async function addNote(title) {
-  const notes = await getNotes()
+  const notes = await getNotes();
   const note = {
     title,
-    id: Date.now().toString()
-  }
+    id: Date.now().toString(),
+  };
 
-  notes.push(note)
+  notes.push(note);
 
-  await saveNotes(notes)
-  console.log(chalk.bgGreen('Note was added!'))
+  await saveNotes(notes);
+  console.log(chalk.bgGreen("Note was added!"));
 }
 
 async function getNotes() {
-  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  let removeNote = null;
+  const newNotes = notes.filter((item) => {
+    if (item.id === id) {
+      removeNote = item;
+    }
+    return item.id !== id;
+  });
+
+  saveNotes(newNotes);
+  if (removeNote) {
+    console.log(
+      chalk.bgRed(`${removeNote.id} ${removeNote.title} - запись удалена`)
+    );
+  }
 }
 
 async function saveNotes(notes) {
-  await fs.writeFile(notesPath, JSON.stringify(notes))
+  await fs.writeFile(notesPath, JSON.stringify(notes));
 }
 
 async function printNotes() {
-  const notes = await getNotes()
+  const notes = await getNotes();
 
-  console.log(chalk.bgBlue('Here is the list of notes:'))
-  notes.forEach(note => {
-    console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
-  })
+  console.log(chalk.bgBlue("Here is the list of notes:"));
+  notes.forEach((note) => {
+    console.log(chalk.bgWhite(note.id), chalk.blue(note.title));
+  });
 }
 
 module.exports = {
-  addNote, printNotes
-}
+  addNote,
+  printNotes,
+  removeNote,
+};
